648d81cdf2c609c7219face191aa3798afd431f0
JoaoBacano <91315939+JoaoBacano@users.noreply.github.com>
2024-04-16 11:56:01 +0100

    lexer and parser

---
+++ mc/CodeAnalysis/ParenthesizedExpressionSyntax.cs
@@ -0,0 +1,26 @@
+namespace Minsk.CodeAnalysis
+{
+    sealed class ParenthesizedExpressionSyntax : ExpressionSyntax
+    {
+
+        public ParenthesizedExpressionSyntax(SyntaxToken openParenthesisToken, ExpressionSyntax expression, SyntaxToken closeParenthesisToken)
+        {
+            OpenParenthesisToken = openParenthesisToken;
+            Expression = expression;
+            CloseParenthesisToken = closeParenthesisToken;
+        }
+        public override SyntaxKind Kind => SyntaxKind.ParenthesizedExpression;
+        public SyntaxToken CloseParenthesisToken { get; }
+        public SyntaxToken OpenParenthesisToken { get; }
+        public ExpressionSyntax Expression { get; }
+
+
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return OpenParenthesisToken;
+            yield return Expression;
+            yield return CloseParenthesisToken;
+        }
+    }
+}
